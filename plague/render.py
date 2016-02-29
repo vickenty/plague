b1ad01c18a1a15742a89d186d95430450b5acbc6
Vickenty Fesunov <kent@setattr.net>
2016-02-29 20:41:40 +0100

    Add a simple map renderer for debuging sim.

---
+++ plague/render.py
@@ -0,0 +1,40 @@
+import pygame
+from pygame.locals import *
+
+class Renderer (object):
+    max_pop = 30
+
+    def draw(self, m, surf):
+        for x in xrange(0, m.width):
+            for y in xrange(0, m.height):
+                rect = (x * 16, y * 16, 14, 14)
+                color = self.get_color(m.grid[x, y])
+                surf.fill(color, rect)
+
+    def get_color(self, cell):
+        return (
+            min(255, int(cell.pop.good ** 0.5 / self.max_pop * 255)),
+            min(255, int(cell.pop.sick ** 0.5 / self.max_pop * 255)),
+            min(255, int(cell.pop.dead ** 0.5 / self.max_pop * 255)),
+        )
+
+if __name__ == '__main__':
+    from sim import Map
+    pygame.init()
+    disp = pygame.display.set_mode((800, 600), DOUBLEBUF)
+    clock = pygame.time.Clock()
+    font = pygame.font.Font(pygame.font.get_default_font(), 16)
+
+    model = Map()
+    model.populate()
+
+    renderer = Renderer()
+
+    while 1:
+        clock.tick(6000)
+        disp.fill(0)
+        model.update()
+        renderer.draw(model, disp)
+        fps = font.render("%.2f" % clock.get_fps(), False, (255, 255, 255))
+        disp.blit(fps, (600, 2))
+        pygame.display.flip()
