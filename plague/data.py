a862b38fd13179b9d545a40d52a1e0f7d4c8af89
Vickenty Fesunov <kent@setattr.net>
2016-03-01 21:21:08 +0100

    Simple data finder and loader.

---
+++ plague/data.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from contextlib import contextmanager
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+data_dirs = [
+    "data",
+    os.path.join(this_dir, "..", "data"),
+]
+
+data_dir = None
+for path in data_dirs:
+    try:
+        with open(os.path.join(path, "plague.magic")):
+            data_dir = path
+            break
+    except IOError:
+        pass
+
+if data_dir is None:
+    print "Sorry, can't find game data. Please run it from the directory where run_game.py is located."
+    sys.exit(1)
+
+def get_path(name):
+    return os.path.join(data_dir, name)
+
+@contextmanager
+def open(name):
+    with file(get_path(name)) as f:
+        yield f
