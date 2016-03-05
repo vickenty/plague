ecb95269925a42eaf02607a08e7bee4aaa850b86
ksurent <ksurent@gmail.com>
2016-03-05 14:24:39 +0100

    autopep8

--- soundtrack/test3d.py
+++ soundtrack/test3d.py
@@ -1,4 +1,6 @@
-import sys, pygame, d3music
+import sys
+import pygame
+import d3music
 from pygame.locals import *
 
 pygame.init()
@@ -11,39 +13,40 @@ d3music.init()
 
 d3music.create()
 
-axes = [ 0, 1, 1 ]
+axes = [0, 1, 1]
 
-d3music.start( axes )
+d3music.start(axes)
 
-_controls = [ {
-    K_q : -20,
-    K_w : -10,
-    K_e : -5,
-    K_r : 5,
-    K_t : 10,
-    K_y : 20
+_controls = [{
+    K_q: -20,
+    K_w: -10,
+    K_e: -5,
+    K_r: 5,
+    K_t: 10,
+    K_y: 20
 }, {
-    K_a : -20,
-    K_s : -10,
-    K_d : -5,
-    K_f : 5,
-    K_g : 10,
-    K_h : 20
+    K_a: -20,
+    K_s: -10,
+    K_d: -5,
+    K_f: 5,
+    K_g: 10,
+    K_h: 20
 }, {
-    K_z : -20,
-    K_x : -10,
-    K_c : -5,
-    K_v : 5,
-    K_b : 10,
-    K_n : 20
-} ]
+    K_z: -20,
+    K_x: -10,
+    K_c: -5,
+    K_v: 5,
+    K_b: 10,
+    K_n: 20
+}]
 
-def process( ev, ax ):
+
+def process(ev, ax):
     changes = 0
 
-    for i, val in enumerate( _controls ):
+    for i, val in enumerate(_controls):
         if ev.key in val:
-            ax[i] += val[ ev.key ] / 100.0
+            ax[i] += val[ev.key] / 100.0
 
             changes = 1
 
@@ -53,7 +56,7 @@ def process( ev, ax ):
             ax[i] = 0
 
     if changes:
-        print "\nmood: %.2f, complexity: %.2f, energy: %.2f" % ( ax[0], ax[1], ax[2] )
+        print "mood: %.2f complexity: %.2f energy: %.2f" % ax[0], ax[1], ax[2]
 
     return changes
 
@@ -62,7 +65,5 @@ while 1:
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if process( event, axes ):
-                d3music.adjust( axes )
-
-
+            if process(event, axes):
+                d3music.adjust(axes)
